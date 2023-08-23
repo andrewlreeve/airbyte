62811f797fb1228f4ac96d0fed80faf404ca9ff8
andrewlreeve <andrew@andrewlreeve.com>
2023-08-23 12:20:04 +1200

    fix: current version

--- airbyte-integrations/connectors/source-cin7-core/source_cin7_core/source.py
+++ airbyte-integrations/connectors/source-cin7-core/source_cin7_core/source.py
@@ -2,7 +2,6 @@
 # Copyright (c) 2023 Airbyte, Inc., all rights reserved.
 #
 
-
 from abc import ABC
 from typing import Any, Iterable, List, Mapping, MutableMapping, Optional, Tuple
 
@@ -11,91 +10,50 @@ from requests.auth import AuthBase
 from airbyte_cdk.models import SyncMode 
 from airbyte_cdk.sources import AbstractSource
 from airbyte_cdk.sources.streams import Stream
-from airbyte_cdk.sources.streams.http import HttpStream
+from airbyte_cdk.sources.streams.http import HttpStream, HttpSubStream
 from airbyte_cdk.sources.streams.http.requests_native_auth.abstract_token import AbstractHeaderAuthenticator 
 
 """
-TODO: Most comments in this class are instructive and should be deleted after the source is implemented.
-
-This file provides a stubbed example of how to use the Airbyte CDK to develop both a source connector which supports full refresh or and an
-incremental syncs from an HTTP API.
-
-The various TODOs are both implementation hints and steps - fulfilling all the TODOs should be sufficient to implement one basic and one incremental
-stream from a source. This pattern is the same one used by Airbyte internally to implement connectors.
-
-The approach here is not authoritative, and devs are free to use their own judgement.
-
-There are additional required TODOs in the files within the integration_tests folder and the spec.yaml file.
+TODO: There are additional required TODOs in the files within the integration_tests folder.
 """
 
 
-# Basic full refresh stream
 class Cin7CoreStream(HttpStream, ABC):
-    """
-    TODO remove this comment
-
-    This class represents a stream output by the connector.
-    This is an abstract base class meant to contain all the common functionality at the API level e.g: the API base URL, pagination strategy,
-    parsing responses etc..
-
-    Each stream should extend this class (or another abstract subclass of it) to specify behavior unique to that stream.
-
-    Typically for REST APIs each stream corresponds to a resource in the API. For example if the API
-    contains the endpoints
-        - GET v1/customers
-        - GET v1/employees
-
-    then you should have three classes:
-    `class Cin7CoreStream(HttpStream, ABC)` which is the current class
-    `class Customers(Cin7CoreStream)` contains behavior to pull data for customers using v1/customers
-    `class Employees(Cin7CoreStream)` contains behavior to pull data for employees using v1/employees
-
-    If some streams implement incremental sync, it is typical to create another class
-    `class IncrementalCin7CoreStream((Cin7CoreStream), ABC)` then have concrete stream implementations extend it. An example
-    is provided below.
-
-    See the reference docs for the full list of configurable options.
-    """
-
-    # TODO: Fill in the url base. Required.
     url_base = "https://inventory.dearsystems.com/ExternalApi/v2/"
 
     def next_page_token(self, response: requests.Response) -> Optional[Mapping[str, Any]]:
-        """
-        TODO: Override this method to define a pagination strategy. If you will not be using pagination, no action is required - just return None.
-
-        This method should return a Mapping (e.g: dict) containing whatever information required to make paginated requests. This dict is passed
-        to most other methods in this class to help you form headers, request bodies, query params, etc..
-
-        For example, if the API accepts a 'page' parameter to determine which page of the result to return, and a response from the API contains a
-        'page' number, then this method should probably return a dict {'page': response.json()['page'] + 1} to increment the page count by 1.
-        The request_params method should then read the input next_page_token and set the 'page' param to next_page_token['page'].
-
-        :param response: the most recent response from the API
-        :return If there is another page in the result, a mapping (e.g: dict) containing information needed to query the next page in the response.
-                If there are no more pages in the result, return None.
-        """
         return None
     
     def backoff_time(self, response: requests.Response) -> Optional[float]:
-        """This method will back off for 65 seconds which is above the current timeout of 60 seconds for the API"""
-        return 65
+        """
+        Back off for 61 seconds which is above the current timeout of 60 seconds for the API
+        """
+        return 61
 
     def request_params(
         self, stream_state: Mapping[str, Any], stream_slice: Mapping[str, any] = None, next_page_token: Mapping[str, Any] = None
     ) -> MutableMapping[str, Any]:
-        """
-        TODO: Override this method to define any query parameters to be set. Remove this method if you don't need to define request params.
-        Usually contains common params e.g. pagination size etc.
-        """
         return {}
 
     def parse_response(self, response: requests.Response, **kwargs) -> Iterable[Mapping]:
-        """
-        TODO: Override this method to define how a response is parsed.
-        :return an iterable containing each record in the response
-        """
         yield {}
+
+
+# Basic incremental stream
+class IncrementalCin7CoreStream(Cin7CoreStream, ABC):
+    state_checkpoint_interval = 1000
+
+    @property
+    def cursor_field(self) -> str:
+        return True
+
+    def get_updated_state(self, current_stream_state: MutableMapping[str, Any], latest_record: Mapping[str, Any]) -> Mapping[str, Any]:
+        current = current_stream_state.get(self.cursor_field, 0)
+        latest = latest_record.get(self.cursor_field)
+
+        return {
+            self.cursor_field: max(current, latest)
+        }
 
 
 class Customers(Cin7CoreStream):
@@ -121,84 +79,155 @@ class Customers(Cin7CoreStream):
             total_records = response.json()["Total"]
             current_page = response.json()["Page"]
 
-            if current_page < total_records / 1000: # TODO change to constant for request params too.
+            if current_page < total_records / 1000:
+                return {"page": current_page + 1}
+
+        return None
+
+    def parse_response(self, response: requests.Response, **kwargs) -> Iterable[Mapping]:
+        return response.json()["CustomerList"]
+
+
+class Sales(IncrementalCin7CoreStream):
+    cursor_field = "UpdatedSince"
+    primary_key = "SaleID"
+    use_cache = True
+
+
+    def __init__(self, config: Mapping[str, Any], **kwargs):
+        super().__init__(**kwargs)
+        self.sales_created_since = config["sales_created_since"]
+
+    def path(
+        self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
+    ) -> str:
+        return "saleList"
+
+    def request_params(
+            self, stream_state: Mapping[str, Any], stream_slice: Mapping[str, any] = None, next_page_token: Mapping[str, Any] = None, start_date: str = None
+    ) -> MutableMapping[str, Any]:
+
+        params = { "limit": 1000 }
+
+        if next_page_token:
+            params.update(next_page_token)
+
+        return params
+
+    def get_updated_state(self, current_stream_state: MutableMapping[str, Any], latest_record: Mapping[str, Any]) -> Mapping[str, Any]:
+        start = self.sales_created_since
+        current = current_stream_state.get(self.cursor_field, "")
+        latest = latest_record.get(self.cursor_field, "")
+
+        return {
+            self.cursor_field: max(current, latest, start)
+        }
+
+    def next_page_token(self, response: requests.Response) -> Optional[Mapping[str, Any]]:
+        if response.json():
+            total_records = response.json()["Total"]
+            current_page = response.json()["Page"]
+
+            if current_page < total_records / 1000:
                 return {"page": current_page + 1}
 
         return None
 
 
     def parse_response(self, response: requests.Response, **kwargs) -> Iterable[Mapping]:
-        return response.json()["CustomerList"]
+        return response.json()["SaleList"]
 
 
-# Basic incremental stream
-class IncrementalCin7CoreStream(Cin7CoreStream, ABC):
-    """
-    TODO fill in details of this class to implement functionality related to incremental syncs for your connector.
-         if you do not need to implement incremental sync for any streams, remove this class.
-    """
+class SaleDetails(HttpSubStream, Sales):
+    primary_key = "ID"
 
-    # TODO: Fill in to checkpoint stream reads after N records. This prevents re-reading of data if the stream fails for any reason.
-    state_checkpoint_interval = None
+    def __init__(self, **kwargs):
+        super().__init__(Sales(**kwargs), **kwargs)
 
-    @property
-    def cursor_field(self) -> str:
-        """
-        TODO
-        Override to return the cursor field used by this stream e.g: an API entity might always use created_at as the cursor field. This is
-        usually id or date based. This field's presence tells the framework this in an incremental stream. Required for incremental.
+    def path(
+        self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
+    ) -> str:
+        return "sale"
 
-        :return str: The name of the cursor field.
-        """
-        return []
+    def request_params(
+        self, stream_state: Mapping[str, Any], stream_slice: Mapping[str, any], **kwargs
+    ) -> MutableMapping[str, Any]:
+        params = { "ID": stream_slice["parent"]["SaleID"] }
 
-    def get_updated_state(self, current_stream_state: MutableMapping[str, Any], latest_record: Mapping[str, Any]) -> Mapping[str, Any]:
-        """
-        Override to determine the latest state after reading the latest record. This typically compared the cursor_field from the latest record and
-        the current state and picks the 'most' recent cursor. This is how a stream's state is determined. Required for incremental.
-        """
-        return {}
+        return params
+
+    def next_page_token(self, response: requests.Response) -> Optional[Mapping[str, Any]]:
+        return None
+
+    def parse_response(self, response: requests.Response, **kwargs) -> Iterable[Mapping]:
+        return [response.json()]
 
 
-class Employees(IncrementalCin7CoreStream):
-    """
-    TODO: Change class name to match the table/data source this stream corresponds to.
-    """
+class SaleCreditNotes(Cin7CoreStream):
+    primary_key = "SaleID"
 
-    # TODO: Fill in the cursor_field. Required.
-    cursor_field = "start_date"
+    def path(
+        self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
+    ) -> str:
+        return "saleCreditNoteList"
 
-    # TODO: Fill in the primary key. Required. This is usually a unique field in the stream, like an ID or a timestamp.
-    primary_key = "employee_id"
+    def request_params(
+        self, stream_state: Mapping[str, Any], stream_slice: Mapping[str, any] = None, next_page_token: Mapping[str, Any] = None
+    ) -> MutableMapping[str, Any]:
+        params = { "limit": 1000 }
 
-    def path(self, **kwargs) -> str:
-        """
-        TODO: Override this method to define the path this stream corresponds to. E.g. if the url is https://example-api.com/v1/employees then this should
-        return "single". Required.
-        """
-        return "employees"
+        if next_page_token:
+            params.update(next_page_token)
 
-    def stream_slices(self, stream_state: Mapping[str, Any] = None, **kwargs) -> Iterable[Optional[Mapping[str, any]]]:
-        """
-        TODO: Optionally override this method to define this stream's slices. If slicing is not needed, delete this method.
+        return params
 
-        Slices control when state is saved. Specifically, state is saved after a slice has been fully read.
-        This is useful if the API offers reads by groups or filters, and can be paired with the state object to make reads efficient. See the "concepts"
-        section of the docs for more information.
+    def next_page_token(self, response: requests.Response) -> Optional[Mapping[str, Any]]:
+        if response.json():
+            total_records = response.json()["Total"]
+            current_page = response.json()["Page"]
 
-        The function is called before reading any records in a stream. It returns an Iterable of dicts, each containing the
-        necessary data to craft a request for a slice. The stream state is usually referenced to determine what slices need to be created.
-        This means that data in a slice is usually closely related to a stream's cursor_field and stream_state.
+            if current_page < total_records / 1000:
+                return {"page": current_page + 1}
 
-        An HTTP request is made for each returned slice. The same slice can be accessed in the path, request_params and request_header functions to help
-        craft that specific request.
+        return None
 
-        For example, if https://example-api.com/v1/employees offers a date query params that returns data for that particular day, one way to implement
-        this would be to consult the stream state object for the last synced date, then return a slice containing each date from the last synced date
-        till now. The request_params function would then grab the date from the stream_slice and make it part of the request by injecting it into
-        the date query param.
-        """
-        raise NotImplementedError("Implement stream slices or delete this method!")
+
+    def parse_response(self, response: requests.Response, **kwargs) -> Iterable[Mapping]:
+        return response.json()["SaleList"]
+
+
+class Products(Cin7CoreStream):
+    primary_key = "ID"
+
+    def path(
+        self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
+    ) -> str:
+        return "product"
+
+    def request_params(
+        self, stream_state: Mapping[str, Any], stream_slice: Mapping[str, any] = None, next_page_token: Mapping[str, Any] = None
+    ) -> MutableMapping[str, Any]:
+        params = { "limit": 1000 }
+
+        if next_page_token:
+            params.update(next_page_token)
+
+        return params
+
+    def next_page_token(self, response: requests.Response) -> Optional[Mapping[str, Any]]:
+        if response.json():
+            total_records = response.json()["Total"]
+            current_page = response.json()["Page"]
+
+            if current_page < total_records / 1000:
+                return {"page": current_page + 1}
+
+        return None
+
+
+    def parse_response(self, response: requests.Response, **kwargs) -> Iterable[Mapping]:
+        return response.json()["Products"]
+
 
 # Connnection class
 # TODO: extract to own file
@@ -243,11 +272,5 @@ class SourceCin7Core(AbstractSource):
             return False, e
 
     def streams(self, config: Mapping[str, Any]) -> List[Stream]:
-        """
-        TODO: Replace the streams below with your own streams.
-
-        :param config: A Mapping of the user input configuration as defined in the connector spec.
-        """
-        # TODO remove the authenticator if not required.
-        auth = Cin7TokenAuthenticator(config)  # Oauth2Authenticator is also available if you need oauth support
-        return [Customers(authenticator=auth), Employees(authenticator=auth)]
+        auth = Cin7TokenAuthenticator(config)
+        return [Customers(authenticator=auth), Sales(authenticator=auth, config=config), SaleDetails(config=config, authenticator=auth), SaleCreditNotes(authenticator=auth), Products(authenticator=auth)]
